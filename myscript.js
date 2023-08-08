5b88fcc9542c7d0dcdadb69b12dd40517549595f
Daniel Borke <dborke36@gmail.com>
2023-08-08 20:15:26 +0000

    java folder

---
+++ myscript.js
@@ -0,0 +1,20 @@
+
+
+let name = ("daniel")
+
+let agbe = -57 == 16
+let y= 72.1 * 29.4
+let ct = 2+3*8+2
+
+
+console.log(name)
+console.log(agbe)
+console.log(y)
+console.log(ct)
+console.log("this is a string")
+
+
+
+
+let x = 10;
+console.log(--x);
