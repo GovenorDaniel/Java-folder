7ed16307522a388ac45765ec6a1fc890564f3552
Daniel Borke <dborke36@gmail.com>
2023-08-22 19:39:29 +0000

    new addition

---
+++ conditionals.js
@@ -0,0 +1,14 @@
+
+function addThreeNumbers(num1,num2,num3){
+
+
+	let answer = num1 + num2 + num3
+
+	return answer
+}
+
+
+console.log("Result of three numbers =")
+
+console.log(addThreeNumbers(5,8,25))
+
